5de2f7974ae1decc30be6f7bc7fbf1aecf57c551
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-10-17 23:54:53 -0300

    Adding comments to posts

--- src/main/java/com/menegheti/workshop/resources/PostResource.java
+++ src/main/java/com/menegheti/workshop/resources/PostResource.java
@@ -1,24 +1,13 @@
 package com.menegheti.workshop.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.menegheti.workshop.domain.Post;
-import com.menegheti.workshop.domain.User;
-import com.menegheti.workshop.dto.UserDTO;
 import com.menegheti.workshop.services.PostService;
 
 @RestController

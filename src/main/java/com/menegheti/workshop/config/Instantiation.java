3a4beb41196409cdabe561eebc17a5c718954a0d
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-10-17 23:15:59 -0300

    Projection of author data with DTO

--- src/main/java/com/menegheti/workshop/config/Instantiation.java
+++ src/main/java/com/menegheti/workshop/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.menegheti.workshop.domain.Post;
 import com.menegheti.workshop.domain.User;
+import com.menegheti.workshop.dto.AuthorDTO;
 import com.menegheti.workshop.repository.PostRepository;
 import com.menegheti.workshop.repository.UserRepository;
 
@@ -35,11 +36,16 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei feliz hoje!", maria);
+		/*primeiro salvar os usuarios e apenas
+		 *depois associar os posts com os autores */
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei feliz hoje!",  new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
 	
 	}
 }

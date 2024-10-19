e19f65ca50857397c3ed3fb6d38ecf2eb4867728
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-10-18 22:41:18 -0300

    Simple query with @Query

--- src/main/java/com/menegheti/workshop/repository/PostRepository.java
+++ src/main/java/com/menegheti/workshop/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.menegheti.workshop.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.menegheti.workshop.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'Title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
